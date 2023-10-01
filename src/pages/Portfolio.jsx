cf6b694b3301e59e6055944edc9a5ac6eaf39b69
godot23 <jasonanavon@gmail.com>
2023-10-01 00:48:27 -0400

    trying to align text on portfolio page, with limited success

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,14 +1,21 @@
-import {useState} from 'react';
-import {Link} from "react-router-dom"
 
-export default function Portfolio(props){
-    return(
+
+export default function Portfolio(props) {
+    return (
         <div className="container text-center">
-            <div className = "list-group">
-                <a href = "https://github.com/KevalPatel6/Jeopardy">Jeopardy Game: Collaboration with Keval Patel and John Doaks </a>
-                <a href = "https://github.com/HarryFelty/BookReview">Book Review Website: Collaboration with Harry Felty and John Doaks</a>
-                <a href = "https://github.com/godot23/Weather_App">Weather App</a>
-                <a href = "https://github.com/godot23/Employee_Database">Employee Database: Backend Only</a> 
+            <div className="list-group">
+                <li >
+                    <a href="https://github.com/KevalPatel6/Jeopardy">Jeopardy Game: Collaboration with Keval Patel and John Doaks </a>
+                </li>
+                <li class = "portfolioItems">
+                    <a href="https://github.com/HarryFelty/BookReview">Book Review Website: Collaboration with Harry Felty and John Doaks</a>
+                </li>
+                <li class = "portfolioItems">
+                    <a href="https://github.com/godot23/Weather_App">Weather App</a>
+                </li>
+                <li class = "portfolioItems">
+                    <a href="https://github.com/godot23/Employee_Database">Employee Database: Backend Only</a>
+                </li>
                 {/* <a href = "placeholder">Project 5</a>
                 <a href = "placeholder">Project 6 </a> */}
             </div>

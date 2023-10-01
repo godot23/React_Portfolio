bd0ea6483166dad445ea3fff45bd32aeeefd4347
godot23 <jasonanavon@gmail.com>
2023-10-01 00:15:01 -0400

    working on the background and header colors

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,9 +1,9 @@
 export default function AboutMe(){
     return(
-        <div>
+        <div id= "aboutMe">
             <h1>About Me </h1>
             <p>
-                Jason Navon is a dev from Pennsylvania. Having graduated from Penn LPS bootcamp, Jason has proficiency in multiple technologies.
+                Jason Navon is a dev from Pennsylvania. Having graduated from Penn LPS bootcamp, he has proficiency in multiple technologies. Prior to this bootcamp, Jason graduated from Drexel University with a BS in psychology, and Villanova University with an MS in experimental psychology.
             </p>
         </div>
     )
